76ecff3d353e7b5218078ed4785ec46591caa286
Muzaffer Bulut <bulutmuzafferr@gmail.com>
2022-12-19 23:02:04 +0300

    hw_1 part 1

---
+++ homework_aqi/js/scripts.js
@@ -0,0 +1,7 @@
+﻿var map = L.map('map').setView([41.1807995,29.1278137], 9);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
