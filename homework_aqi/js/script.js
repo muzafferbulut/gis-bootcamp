78a2b8384595a333a510d0105a04f0d646ac1057
Muzaffer Bulut <bulutmuzafferr@gmail.com>
2022-12-22 00:15:21 +0300

    istasyon listesi eklendi

--- homework_aqi/js/script.js
+++ homework_aqi/js/script.js
@@ -5,6 +5,25 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+const selectElement = document.getElementById('stationList');
+
 fetch('https://api.ibb.gov.tr/havakalitesi/OpenDataPortalHandler/GetAQIStations')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then(response => response.json())
+  .then(data => {
+    // Dönen verileri düzenle ve seç
+    const stations = data.map(station => ({
+      id: station.Id,
+      name: station.Name
+    }));
+
+    // Option elementlerini oluştur ve select elementine ekle
+    stations.forEach(station => {
+      const optionElement = document.createElement("option");
+      optionElement.value = station.id;
+      optionElement.textContent = station.name;
+      selectElement.appendChild(optionElement);
+    });
+  })
+  .catch(error => {
+    console.error('Hata:', error);
+  });
