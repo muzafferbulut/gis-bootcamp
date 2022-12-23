7b24066d312219633e323ec95b5ef6afe5c3944a
Muzaffer Bulut <bulutmuzafferr@gmail.com>
2022-12-23 22:43:08 +0300

    secilen haritaya zoom yapılır

--- homework_aqi/js/script.js
+++ homework_aqi/js/script.js
@@ -12,6 +12,7 @@ const selectElement = document.getElementById('stationList');
 fetch('https://api.ibb.gov.tr/havakalitesi/OpenDataPortalHandler/GetAQIStations')
   .then(response => response.json())
   .then(data => {
+    
     // Dönen verileri düzenle ve seç
     const stations = data.map(station => ({
       id: station.Id,
@@ -24,7 +25,6 @@ fetch('https://api.ibb.gov.tr/havakalitesi/OpenDataPortalHandler/GetAQIStations'
       optionElement.value = station.id;
       optionElement.textContent = station.name;
       selectElement.appendChild(optionElement);
-
     });
 
     data.forEach(station=> {
@@ -34,11 +34,32 @@ fetch('https://api.ibb.gov.tr/havakalitesi/OpenDataPortalHandler/GetAQIStations'
       const lat = parts[1];
 
       L.marker([lat, lng]).addTo(map).bindPopup(station.Name);
-    })
+    });
 
+    selectElement.addEventListener('change', event => {
+      const selectedId = event.target.value;
+      
+      const selectedStation = data.find(station => station.Id === selectedId);
+
+      const selectedStationLoc = selectedStation.Location;
+      const stationLocParts = selectedStationLoc.match(/\((.*)\)/)[1].split(' ');
+      const stationLng = stationLocParts[0];
+      const stationLat = stationLocParts[1];
+
+      const stationDiv = document.getElementById("station-info");
+      stationDiv.innerHTML = `<h3>Station Name : </h3> ${selectedStation.Name}<br>
+      <h3>Station Address : </h3> ${selectedStation.Adress}<br>
+      <h3>Longitude : </h3> ${stationLng}<br>
+      <h3>Latitude : </h3> ${stationLat}<br>`;
+
+      map.setView([stationLat, stationLng], 15);
+
+    });
+
+
+  
 
   })
-  .catch(error => {
+.catch(error => {
     console.error('Hata:', error);
-  });
-
+});
